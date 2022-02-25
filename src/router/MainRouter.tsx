de976810a513588df6ad6c2f79d3a6a11531ca7a
ripterdust <bryantello2010@hotmail.com>
2022-02-24 22:11:33 -0600

    Added the basic router configuration and starting to build the main page

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,8 +1,13 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react'
+import { MainPage } from '../components/MainPage';
 
 export const MainRouter = () => {
-  return (
-    <div>MainRouter</div>
-  )
+  return <BrowserRouter>
+    
+    <Routes>
+        <Route index element={MainPage} />
+    </Routes>
+  
+  </BrowserRouter>
 }
