6341d7302116f69ea36d1e285f1ad7ec1be5a435
ripterdust <bryantello2010@hotmail.com>
2022-03-15 13:22:21 -0600

    Added the main nav layout

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,9 +3,23 @@ import React from 'react';
 export const Nav = () => {
     return (
         <nav>
-            <Link to={'/'}>
+            <Link to={'/'} className="logo">
                 <i className="fa-solid fa-bag-shopping"></i>
             </Link>
+            <div className="categories"></div>
+            <div className="utils">
+                <div className="user">
+                    <i className="fa-solid fa-user"></i>
+                    <div className="menu">
+                        <div className="option">HOla</div>
+                        <div className="option">HOla</div>
+                        <div className="option">HOla</div>
+                    </div>
+                </div>
+                <div className="cart">
+                    <i className="fa-solid fa-basket-shopping"></i>
+                </div>
+            </div>
         </nav>
     );
 };
