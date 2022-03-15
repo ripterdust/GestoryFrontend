1ebac282ddbe9d071f16f45ab215579c9b59b063
ripterdust <bryantello2010@hotmail.com>
2022-03-15 15:05:05 -0600

    Main styles

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -9,18 +9,16 @@ export const MainPage: React.FC = () => {
     return (
         <div className="container">
             <div className="mainpage">
-                <div className="carrousell">
-                    <Link to={'/woman'} className="cover woman">
-                        <img src={coverWoman} alt="Woman photo" />
-                        <div className="overlay"></div>
-                        <div className="text">Womans clothes</div>
-                    </Link>
-                    <Link to={'/men'} className="cover man">
-                        <img src={coverMan} alt="Men Photo" />
-                        <div className="overlay"></div>
-                        <div className="text">Men clothes</div>
-                    </Link>
-                </div>
+                <Link to={'/woman'} className="cover">
+                    <img src={coverWoman} alt="Woman photo" />
+                    <div className="overlay"></div>
+                    <div className="text">Womans clothes</div>
+                </Link>
+                <Link to={'/men'} className="cover mt">
+                    <img src={coverMan} alt="Men Photo" />
+                    <div className="overlay"></div>
+                    <div className="text">Men clothes</div>
+                </Link>
             </div>
         </div>
     );
