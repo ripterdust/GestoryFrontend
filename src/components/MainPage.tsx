08abf5654f27183d6b8756229cc718a99bf2a99b
ripterdust <bryantello2010@hotmail.com>
2022-03-15 15:10:49 -0600

    Added the grid layout

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -9,16 +9,18 @@ export const MainPage: React.FC = () => {
     return (
         <div className="container">
             <div className="mainpage animate__animated animate__fadeInUp">
-                <Link to={'/woman'} className="cover">
-                    <img src={coverWoman} alt="Woman photo" />
-                    <div className="overlay"></div>
-                    <div className="text">Womans clothes</div>
-                </Link>
-                <Link to={'/men'} className="cover mt">
-                    <img src={coverMan} alt="Men Photo" />
-                    <div className="overlay"></div>
-                    <div className="text">Men clothes</div>
-                </Link>
+                <div className="grid">
+                    <Link to={'/woman'} className="cover">
+                        <img src={coverWoman} alt="Woman photo" />
+                        <div className="overlay"></div>
+                        <div className="text">Womans clothes</div>
+                    </Link>
+                    <Link to={'/men'} className="cover mt">
+                        <img src={coverMan} alt="Men Photo" />
+                        <div className="overlay"></div>
+                        <div className="text">Men clothes</div>
+                    </Link>
+                </div>
             </div>
         </div>
     );
