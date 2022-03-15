d4438e5d569e4e8e402f8d2034b4640eee4c7a46
ripterdust <bryantello2010@hotmail.com>
2022-03-15 14:59:33 -0600

    Add the carrousell on the clothes

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import { getUrl } from '../helpers/apiRoute';
 // import { useFetch } from '../hooks/useFetch';
-import cover from '../assets/cover.jpg';
+import coverWoman from '../assets/cover.jpg';
+import coverMan from '../assets/coverMan.jpg';
 
 export const MainPage: React.FC = () => {
     return (
         <div className="container">
             <div className="mainpage">
-                <Link to={'/woman'} className="cover">
-                    <img src={cover} alt="Woman photo" />
-                    <div className="overlay"></div>
-                    <div className="text">Womans clothes</div>
-                </Link>
-                <Link to={'/men'} className="cover mt">
-                    <img src={cover} alt="Men Photo" />
-                    <div className="overlay"></div>
-                    <div className="text">Men clothes</div>
-                </Link>
+                <div className="carrousell">
+                    <Link to={'/woman'} className="cover woman">
+                        <img src={coverWoman} alt="Woman photo" />
+                        <div className="overlay"></div>
+                        <div className="text">Womans clothes</div>
+                    </Link>
+                    <Link to={'/men'} className="cover man">
+                        <img src={coverMan} alt="Men Photo" />
+                        <div className="overlay"></div>
+                        <div className="text">Men clothes</div>
+                    </Link>
+                </div>
             </div>
         </div>
     );
