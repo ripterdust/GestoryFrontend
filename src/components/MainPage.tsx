3d0bf61bcd6a28100e307e128e54b9ec9fdcfaaf
ripterdust <bryantello2010@hotmail.com>
2022-03-15 14:32:08 -0600

    Fixed the covers styles

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -9,16 +9,14 @@ export const MainPage: React.FC = () => {
         <div className="container">
             <div className="mainpage">
                 <Link to={'/woman'} className="cover">
-                    <img src={cover} alt="" />
+                    <img src={cover} alt="Woman photo" />
                     <div className="overlay"></div>
                     <div className="text">Womans clothes</div>
                 </Link>
-            </div>
-            <div className="mainpage">
-                <Link to={'/woman'} className="cover">
-                    <img src={cover} alt="" />
+                <Link to={'/men'} className="cover mt">
+                    <img src={cover} alt="Men Photo" />
                     <div className="overlay"></div>
-                    <div className="text">Womans clothes</div>
+                    <div className="text">Men clothes</div>
                 </Link>
             </div>
         </div>
