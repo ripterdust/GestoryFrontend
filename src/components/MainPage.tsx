3345ad37d607af7d7781ffd40dcef669aded9fd5
ripterdust <bryantello2010@hotmail.com>
2022-03-15 14:01:53 -0600

    Added the facebook img

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { getUrl } from '../helpers/apiRoute';
-import { useFetch } from '../hooks/useFetch';
+// import { getUrl } from '../helpers/apiRoute';
+// import { useFetch } from '../hooks/useFetch';
+import facebook from '../assets/facebook.jpg';
 
 export const MainPage: React.FC = () => {
-    const response = useFetch(getUrl(''));
-
-    let data: Array<object> = [];
-
-    const elem = response.then((x) => (data = x));
-
-    console.log(elem);
-
-    return <div>Main</div>;
+    return (
+        <div>
+            <img src={facebook} alt="" />
+        </div>
+    );
 };
