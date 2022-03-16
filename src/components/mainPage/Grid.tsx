27d565fd01a81b6ace1c62ca1843581d69fc2d97
ripterdust <bryantello2010@hotmail.com>
2022-03-16 02:36:46 -0600

    Added product card

--- src/components/mainPage/Grid.tsx
+++ src/components/mainPage/Grid.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { getUrl } from '../../helpers/apiRoute';
 import { useFetch } from '../../hooks/useFetch';
+import { ProductCard } from './ProductCard';
 
 export const Grid = () => {
     const { data } = useFetch(getUrl(''));
-    return <div className="grid">Acá irá la gria asdf</div>;
+    return (
+        <div className="grid">
+            {data.err ? (
+                <div className="err">Sometimes went wrong</div>
+            ) : (
+                <ProductCard />
+            )}
+        </div>
+    );
 };
