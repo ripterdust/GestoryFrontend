3e9181774ce0f2ae599abefdafaefb48ee349892
ripterdust <bryantello2010@hotmail.com>
2022-03-16 04:38:02 -0600

    Added the grid layout

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export const Login = () => {
-    return <div>Login</div>;
+    return (
+        <div className="login">
+            <div className="loginGrid">
+                <div className="square"></div>
+                <div className="form"></div>
+            </div>
+        </div>
+    );
 };
