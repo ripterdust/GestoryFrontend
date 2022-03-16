f2e01ece08889bce7ee195515fb670f1edb62ba6
ripterdust <bryantello2010@hotmail.com>
2022-03-16 05:01:30 -0600

    Added the login bg

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import cover from '../assets/login.jpg';
 export const Login = () => {
     return (
         <div className="login">
-            <
+            <div className="img">
+                <img src={cover} alt="" />
+            </div>
         </div>
     );
 };
