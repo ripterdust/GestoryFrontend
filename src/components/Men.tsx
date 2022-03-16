92d82a0fd937ae5e80646ece19a9d4a9cd312670
ripterdust <bryantello2010@hotmail.com>
2022-03-16 04:31:56 -0600

    Added the route and men compoent

---
+++ src/components/Men.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Men = () => {
+    return <div>Men</div>;
+};
--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { MainPage } from '../components/MainPage';
 import { Nav } from '../components/Nav';
 import { Login } from '../components/Login';
+import { Men } from '../components/Men';
 
 export const MainRouter: React.FC = () => {
     return (
@@ -11,6 +12,7 @@ export const MainRouter: React.FC = () => {
             <Routes>
                 <Route path="/" element={<MainPage />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/men" element={<Men />} />
             </Routes>
         </div>
     );
