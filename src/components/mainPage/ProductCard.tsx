f4740351ec0ac9e95eef54eacdc46f69912a73a8
ripterdust <bryantello2010@hotmail.com>
2022-03-16 03:08:19 -0600

    Added all the data types

--- src/components/mainPage/ProductCard.tsx
+++ src/components/mainPage/ProductCard.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-interface props {
-    data?: object;
+interface dataType {
+    id: string;
+    productCategory: string;
+    productImage: string;
+    productName: string;
+    productPrice: number;
+    serialNumber: string;
 }
-export const ProductCard: React.FC<props> = (data) => {
+
+type props = {
+    id: string;
+    data: dataType;
+};
+
+export const ProductCard: React.FC<props> = (dataProp) => {
+    const { data } = dataProp;
     console.log(data);
-    return <div>ProductCard</div>;
+    const { id } = data;
+    return <Link to={`/product/${id}`}>Product</Link>;
 };
