96dcd9b09c7e2e31ea457d40a98e669efc0f5cff
ripterdust <bryantello2010@hotmail.com>
2022-03-16 05:06:33 -0600

    Added the login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import cover from '../assets/login.jpg';
 export const Login = () => {
     return (
         <div className="login">
-            <div className="img">
-                <img src={cover} alt="" />
+            <div className="form">
+                <form action="">
+                    <input type="email" name="email" id="" />
+                    <input type="password" name="password" id="" />
+                    <button type="submit">Login</button>
+                </form>
             </div>
         </div>
     );
