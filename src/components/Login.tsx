ba36068bf634aa97ecdd408689ba449f0b8160a0
ripterdust <bryantello2010@hotmail.com>
2022-03-16 04:42:10 -0600

    Deleted the unnecesary text

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Login = () => {
     return (
         <div className="login">
-            <div className="square">asdasd</div>
+            <div className="square"></div>
             <div className="form"></div>
         </div>
     );
