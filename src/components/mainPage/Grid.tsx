f1013a01abfd8ef35d55467d0057641f39174e0e
ripterdust <bryantello2010@hotmail.com>
2022-03-16 03:16:25 -0600

    Code working

--- src/components/mainPage/Grid.tsx
+++ src/components/mainPage/Grid.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { getUrl } from '../../helpers/apiRoute';
 import { useFetch } from '../../hooks/useFetch';
 import { ProductCard } from './ProductCard';
+
 export const Grid = () => {
     const { data } = useFetch(getUrl(''));
     return (
