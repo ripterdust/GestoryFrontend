b0b9c20b6537fcd712c99f43b0cab254acca891b
ripterdust <bryantello2010@hotmail.com>
2022-03-16 15:15:14 -0600

    Addded the quantity data

--- src/components/ProductInfo.tsx
+++ src/components/ProductInfo.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getUrl } from '../helpers/apiRoute';
 import { useFetch } from '../hooks/useFetch';
 
 export const ProductInfo = () => {
+    // Hooks
+    const [state, setState] = useState({
+        productId: '',
+        quantity: 0,
+        size: 'sm',
+    });
+
     // Getting params
     const { id } = useParams();
 
     // Formatting data
     const { data: response } = useFetch(getUrl(`product/${id}`));
     const { data } = response;
-
     // Formating price
     const lang = navigator.language;
     const options = { style: 'currency', currency: 'USD' };
     const intl = new Intl.NumberFormat(lang, options);
     const price = intl.format(data?.productPrice);
+
+    const hanldeClick = (sum: number) => {
+        setState((state) => ({
+            ...state,
+            quantity: state.quantity + sum < 0 ? 0 : state.quantity + sum,
+        }));
+    };
+
     return (
         <div className="animate__animated animate__fadeIn productInfo">
             <div className="title">{data.productName}</div>
@@ -33,6 +47,12 @@ export const ProductInfo = () => {
                 </div>
                 <div className="info">
                     <div className="price">{price}</div>
+                    <div className="quantity">
+                        <button onClick={() => hanldeClick(-1)}>-</button>
+                        <span>{state.quantity}</span>
+                        <button onClick={() => hanldeClick(1)}>+</button>
+                        <button>Add to cart</button>
+                    </div>
                     <div className="description">{data.productDescription}</div>
                 </div>
             </div>
