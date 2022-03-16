2fba74fdadce93f8539fb5af7774874619455601
ripterdust <bryantello2010@hotmail.com>
2022-03-16 05:13:47 -0600

    Added the main layout, added some styles and added the animations

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 export const Login = () => {
     return (
-        <div className="login">
+        <div className="login animate__animated animate__fadeIn">
             <div className="form">
+                <div className="title">
+                    Wellcome to <span>DressU</span>
+                </div>
                 <form action="">
-                    <input type="email" name="email" id="" />
-                    <input type="password" name="password" id="" />
+                    <div className="form-group">
+                        <label htmlFor="">Email</label>
+                        <input type="email" name="email" id="" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="">Password</label>
+                        <input type="password" name="password" id="" />
+                    </div>
                     <button type="submit">Login</button>
                 </form>
             </div>
