3a938d89d58226778c373d0fb5eff04328150343
ripterdust <bryantello2010@hotmail.com>
2022-03-16 13:55:31 -0600

    Added the img on the product info

--- src/components/ProductInfo.tsx
+++ src/components/ProductInfo.tsx
@@ -7,16 +7,23 @@ export const ProductInfo = () => {
     const { id } = useParams();
     const { data: response } = useFetch(getUrl(`product/${id}`));
     const { data } = response;
-    console.log(data);
+
+    // Formating price
+    const lang = navigator.language;
+    const options = { style: 'currency', currency: 'USD' };
+    const intl = new Intl.NumberFormat(lang, options);
+    const price = intl.format(data?.productPrice);
+    const image = data.productImage;
     return (
         <div className="animate__animated animate__fadeIn productInfo">
             <div className="title">{data.productName}</div>
 
             <div className="productGrid">
                 <div className="img">
-                    <img src={`${data.productImage}`} alt="" />
+                    <img src={`data:image/jpeg;base64,${image}`} alt="" />
                 </div>
                 <div className="info">
+                    <div className="price">{price}</div>
                     <div className="description">{data.productDescription}</div>
                 </div>
             </div>
