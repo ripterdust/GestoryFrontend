c6206af4c9633d42eb1682a518506f90e8b825cd
ripterdust <bryantello2010@hotmail.com>
2022-03-16 13:46:18 -0600

    Showing the description

--- src/components/ProductInfo.tsx
+++ src/components/ProductInfo.tsx
@@ -7,10 +7,19 @@ export const ProductInfo = () => {
     const { id } = useParams();
     const { data: response } = useFetch(getUrl(`product/${id}`));
     const { data } = response;
-
+    console.log(data);
     return (
         <div className="animate__animated animate__fadeIn productInfo">
             <div className="title">{data.productName}</div>
+
+            <div className="productGrid">
+                <div className="img">
+                    <img src={`${data.productImage}`} alt="" />
+                </div>
+                <div className="info">
+                    <div className="description">{data.productDescription}</div>
+                </div>
+            </div>
         </div>
     );
 };
