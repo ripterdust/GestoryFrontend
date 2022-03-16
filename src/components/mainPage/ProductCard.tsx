53f9a61ce659c6f42e26deb8b0c0533f65cab9c8
ripterdust <bryantello2010@hotmail.com>
2022-03-16 03:13:52 -0600

    Added the type

--- src/components/mainPage/ProductCard.tsx
+++ src/components/mainPage/ProductCard.tsx
@@ -18,7 +18,6 @@ export type props = {
 
 export const ProductCard: React.FC<props> = (dataProp) => {
     const { data } = dataProp;
-    console.log(data);
     const { id } = data;
     return <Link to={`/product/${id}`}>Product</Link>;
 };
