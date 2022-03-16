638e7c3ccb0c4023141e78bc08d598eefddc7ddc
ripterdust <bryantello2010@hotmail.com>
2022-03-16 03:13:19 -0600

    Fixed the types

--- src/components/mainPage/Grid.tsx
+++ src/components/mainPage/Grid.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { getUrl } from '../../helpers/apiRoute';
 import { useFetch } from '../../hooks/useFetch';
 import { ProductCard } from './ProductCard';
-
 export const Grid = () => {
     const { data } = useFetch(getUrl(''));
     return (
@@ -10,7 +9,7 @@ export const Grid = () => {
             {data.err ? (
                 <div className="err">Sometimes went wrong</div>
             ) : (
-                data.data.map((product: object, key: number) => (
+                data.data.map((product: any, key: number) => (
                     <ProductCard key={key} data={product} />
                 ))
             )}
--- src/components/mainPage/ProductCard.tsx
+++ src/components/mainPage/ProductCard.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+export interface dataType {
+    id: string;
+    productCategory: string;
+    productImage: string;
+    productName: string;
+    productPrice: number;
+    serialNumber: string;
+}
+
+export type props = {
+    data?: dataType;
+    key?: number;
+    id: string;
+};
+
 export const ProductCard: React.FC<props> = (dataProp) => {
     const { data } = dataProp;
     console.log(data);
