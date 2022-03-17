2c87d8cc004ad9a57d5546181e0b86aa7c12900c
ripterdust <bryantello2010@hotmail.com>
2022-03-17 12:59:25 -0600

    Added the localstorage cart element

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { MainRouter } from './router/MainRouter';
 import React from 'react';
 
 export const App: React.FC = () => {
+    if (!localStorage.cart) {
+        localStorage.cart = [];
+    }
+
     return (
         <BrowserRouter>
             <MainRouter />
