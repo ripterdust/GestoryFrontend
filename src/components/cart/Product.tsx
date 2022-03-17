c1332aab913a9fece4000c6572d7a79d577f5ad5
ripterdust <bryantello2010@hotmail.com>
2022-03-17 15:24:27 -0600

    Added the formated price

--- src/components/cart/Product.tsx
+++ src/components/cart/Product.tsx
@@ -11,10 +11,16 @@ interface props {
 }
 
 export const Product: React.FC<props> = ({ data }) => {
+    const { name, price } = data;
+
+    const lang = navigator.language;
+    const options = { style: 'currency', currency: 'USD' };
+    const intl = new Intl.NumberFormat(lang, options);
+    const formatedPrice = intl.format(price);
     return (
         <div className="cart-item">
-            <span className="name">{data.name}</span>
-            <span className="price">{data.price}</span>
+            <span className="name">{name}</span>
+            <span className="price">{formatedPrice}</span>
         </div>
     );
 };
