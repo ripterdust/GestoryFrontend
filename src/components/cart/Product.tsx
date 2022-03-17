58f18a40fa7379fcc652f9ccead4ed66f3e1945f
ripterdust <bryantello2010@hotmail.com>
2022-03-17 14:56:59 -0600

    Added the data type on the functional component

--- src/components/cart/Product.tsx
+++ src/components/cart/Product.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export const Product: React.FC = () => {
+interface dataType {
+    name: string;
+    price: number;
+    quantity: number;
+}
+interface props {
+    key: number;
+    data: dataType;
+}
+
+export const Product: React.FC<props> = () => {
     return <div>Product</div>;
 };
