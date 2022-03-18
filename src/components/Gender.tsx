ec9ff1f409dc35dc4e17b98c3cc0974b91c8f614
ripterdust <bryantello2010@hotmail.com>
2022-03-18 15:33:58 -0600

    Processing the gender

--- src/components/Gender.tsx
+++ src/components/Gender.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export const Gender = () => {
-    return <div>Gender</div>;
+    const { gender } = useParams();
+    return <div>{gender}</div>;
 };
