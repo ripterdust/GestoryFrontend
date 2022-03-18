209761ffabd517ad56b23cda558e7d9b841aa970
ripterdust <bryantello2010@hotmail.com>
2022-03-18 15:36:08 -0600

    Fetching all the products by gender

--- src/components/Gender.tsx
+++ src/components/Gender.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { getUrl } from '../helpers/apiRoute';
+import { useFetch } from '../hooks/useFetch';
 
 export const Gender = () => {
     const { gender } = useParams();
+    const data = useFetch(getUrl(`gender/${gender}`));
+    console.log(data);
     return <div>{gender}</div>;
 };
