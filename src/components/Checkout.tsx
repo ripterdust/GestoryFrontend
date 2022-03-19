011431297c175a3a1ebd95ee67f238e871339ccd
ripterdust <bryantello2010@hotmail.com>
2022-03-18 22:55:59 -0600

    Added the totalSaleAmount and rendering the product

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { getCartData } from './cart/getCartData';
+import { Product } from './checkout/Product';
 
 interface productType {
     name: string;
     price: number;
     image: any;
+    quantity: number;
 }
 export const Checkout: React.FC = () => {
     const cartObject = JSON.parse(localStorage.cart);
     const cart = getCartData(cartObject);
+    let totalSaleAmount = 0;
     return (
         <div className="checkout">
             <div className="items">
-                {cart.map((product: productType) => product.name)}
+                {cart.map((product: productType, key: number) => {
+                    totalSaleAmount += product.price * product.quantity;
+                    return <Product key={key} />;
+                })}
             </div>
+            {totalSaleAmount}
         </div>
     );
 };
