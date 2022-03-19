7a281ffa7e5a9a615029c282c2cadb5b8be53162
ripterdust <bryantello2010@hotmail.com>
2022-03-18 22:08:26 -0600

    Added the context

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter } from 'react-router-dom';
 import { MainRouter } from './router/MainRouter';
 import React from 'react';
-
+import { ReloadContext } from './contexts/useReloadContext';
 export const App: React.FC = () => {
     if (!localStorage.cart) {
         localStorage.cart = JSON.stringify({});
     }
 
     return (
-        <BrowserRouter>
-            <MainRouter />
-        </BrowserRouter>
+        <ReloadContext.Provider value={true}>
+            <BrowserRouter>
+                <MainRouter />
+            </BrowserRouter>
+        </ReloadContext.Provider>
     );
 };
--- src/contexts/useReloadContext.tsx
+++ src/contexts/useReloadContext.tsx
@@ -1,3 +1,3 @@
 import { createContext } from 'react';
 
-export const useReloadContext = createContext(true);
+export const ReloadContext = createContext(true);
