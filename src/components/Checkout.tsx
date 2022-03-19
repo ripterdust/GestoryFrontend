bf92775e56c2d2f1c86f330d44662722b49eedb7
ripterdust <bryantello2010@hotmail.com>
2022-03-18 22:51:04 -0600

    Added the product query

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { getCartData } from './cart/getCartData';
 
+interface productType {
+    name: string;
+    price: number;
+    image: any;
+}
 export const Checkout: React.FC = () => {
-    const cart = JSON.parse(localStorage.cart);
-    const cartArray = getCartData(cart);
-    console.log(cartArray);
-    return <div>Checkout</div>;
+    const cartObject = JSON.parse(localStorage.cart);
+    const cart = getCartData(cartObject);
+    return (
+        <div className="checkout">
+            <div className="items">
+                {cart.map((product: productType) => product.name)}
+            </div>
+        </div>
+    );
 };
