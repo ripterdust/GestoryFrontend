52a1b388048a58ef2da8b54effbb70e2291467b6
ripterdust <bryantello2010@hotmail.com>
2022-03-18 22:02:17 -0600

    Added the typescript component data type

--- src/components/Gender.tsx
+++ src/components/Gender.tsx
@@ -4,7 +4,7 @@ import { getUrl } from '../helpers/apiRoute';
 import { useFetch } from '../hooks/useFetch';
 import { Grid } from './mainPage/Grid';
 
-export const Gender = () => {
+export const Gender: React.FC = () => {
     const { gender } = useParams();
     const { data } = useFetch(getUrl(`gender/${gender}`));
     return <Grid data={data} />;
