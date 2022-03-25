5c3d9d866df7560b8d56a0ab7c31e1322bf207f5
ripterdust <bryantello2010@hotmail.com>
2022-03-25 14:25:05 -0600

    Added the pay navigation

--- src/components/checkout/Check.tsx
+++ src/components/checkout/Check.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { formatePrice } from '../../helpers/formatePrice';
 
 export const Check: React.FC = () => {
-    const cart = JSON.parse(localStorage.cart);
+    const navigate = useNavigate();
 
+    const cart = JSON.parse(localStorage.cart);
     let total = 0;
     Object.keys(cart).map((key) => {
         const { price, quantity } = cart[key];
         total += price * quantity;
     });
-
     const formatedTotal = formatePrice(total);
+
+    const buttonHandler = () => {
+        navigate('/pay', { replace: true });
+    };
+
     return (
         <div>
             <span>Total: {formatedTotal}</span>
-            <button>Checkout</button>
+            <button onClick={buttonHandler}>Checkout</button>
         </div>
     );
 };
