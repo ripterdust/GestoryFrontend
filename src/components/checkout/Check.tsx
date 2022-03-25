e687b26238db85b811c26101917dedc8a88950a2
ripterdust <bryantello2010@hotmail.com>
2022-03-25 13:42:23 -0600

    Added the total on the checkout

--- src/components/checkout/Check.tsx
+++ src/components/checkout/Check.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { formatePrice } from '../../helpers/formatePrice';
 
 export const Check: React.FC = () => {
-    return <div>Check</div>;
+    const cart = JSON.parse(localStorage.cart);
+
+    let total = 0;
+    Object.keys(cart).map((key) => {
+        const { price, quantity } = cart[key];
+        total += price * quantity;
+    });
+
+    const formatedTotal = formatePrice(total);
+    return (
+        <div>
+            <span>Total: {formatedTotal}</span>
+            <button>Checkout</button>
+        </div>
+    );
 };
