2f2ea4f37622c3a8f039eef47c8e02c043d1b3d8
ripterdust <bryantello2010@hotmail.com>
2022-04-03 14:20:12 -0600

    Finished post data

--- src/helpers/account/postForm.ts
+++ src/helpers/account/postForm.ts
@@ -1,3 +1,8 @@
-export const postForm = (data: any) => {
+import axios from 'axios';
+
+export const postForm = async (formData: any) => {
+    const baseUrl = 'http://localhost:3000/api';
+    const url = `${baseUrl}/signin`;
+    const { data } = await axios.post(url);
     console.log(data);
 };
