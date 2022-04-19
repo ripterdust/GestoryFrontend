cc704d6a1b9d1f65da8378901bf5a6f891a20d3d
ripterdust <bryantello2010@hotmail.com>
2022-04-18 19:29:59 -0600

    Added the remote api link

--- src/helpers/apiRoute.ts
+++ src/helpers/apiRoute.ts
@@ -1,4 +1,4 @@
 export const getUrl = (site: string): string => {
-    const url = `http://localhost:3000/api/${site}`;
+    const url = `https://dress-u.herokuapp.com/api/${site}`;
     return url;
 };
